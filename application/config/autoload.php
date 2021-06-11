ffdcd5138c81530ffc922197dd20334aec112a91
Fiqri Muhammad Zuhair Pohan <fzuhairpohan@gmail.com>
2021-06-12 01:18:24 +0700

    Database,dashboard,daftar modul

--- application/config/autoload.php
+++ application/config/autoload.php
@@ -89,7 +89,7 @@ $autoload['drivers'] = array();
 |
 |	$autoload['helper'] = array('url', 'file');
 */
-$autoload['helper'] = array('form','url','string');
+$autoload['helper'] = array('form','url','cekModul_helper','cekModulPengguna_helper','cekParentModul_helper','cekParentModulPengguna_helper','activity_helper','string');
 
 /*
 | -------------------------------------------------------------------
---
+++ application/controllers/panel/Dashboard.php
@@ -0,0 +1,23 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+class Dashboard extends CI_Controller {
+
+	public $parent_modul = 'Dashboard';
+	public $title = 'Dashboard';
+
+	public function __construct(){
+		parent::__construct();
+		if($this->session->userdata('LoggedIN') == FALSE ) redirect('auth/logout');
+	}
+
+	public function index()
+	{
+		$data['title'] = $this->title;
+		$data['subtitle'] = 'Dashboard';
+		$data['content'] = 'panel/dashboard/index';
+		$data['jmlPengguna'] = $this->GeneralModel->count_general('e_pengguna');
+		$this->load->view('panel/content', $data);
+	}
+
+}
